57accf877750495e729625ed7a6036234220f29c
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-01-15 14:20:16 -0500

    Made owl carousel faster

--- app/assets/javascripts/owl.js
+++ app/assets/javascripts/owl.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
  
       slideSpeed : 300,
       paginationSpeed : 400,
+      autoPlay: true,
       singleItem:true,
       autoHeight : true,
       navigation: false,
